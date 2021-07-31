c37e0eae34480c3c466ccf96b4a14121bfa929ad
fayyozbobokulov <b.fayyoz2000@gmail.com>
2021-07-31 10:49:39 +0500

    Updating

--- src/app.ts
+++ src/app.ts
@@ -1,2 +1,18 @@
-// Code goes here!
+class ProjectInput {
+    templateElement: HTMLTemplateElement;
+    hostElement: HTMLDivElement;
+    element: HTMLFormElement;
 
+    constructor() {
+        this.templateElement = document.getElementById('project-input')! as HTMLTemplateElement;
+        this.hostElement = document.getElementById('app')! as HTMLDivElement;
+
+        const importedNode = document.importNode(this.templateElement.content, true);
+
+        this.element = importedNode.firstElementChild as HTMLFormElement;
+    }
+
+    private attach() {
+        this.hostElement.insertAdjacentElement('afterbegin', );
+    }
+}
